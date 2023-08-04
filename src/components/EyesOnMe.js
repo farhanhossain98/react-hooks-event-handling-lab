4ceeade0aa4b793ed09163e3d1390c7bebe86320
farhan hossain <farhanrufon99@gmail.com>
2023-08-03 22:28:44 -0400

    changes to componenets

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe() {
+    function handleFocus() {
+        console.log('Good!')
+    }
+    function handleBlur() {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
+
+//We first start with the eyesonme function and then add two handle functions
+//One for the blur and the other for the focus whic we will name handleBlur and handleFocus
+//Each one has a console.log accordingly
+// What we return is the button witht eh on"handle" to which is blur and focus with the given text in the button tag
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+
+function Keypad() {
+    function handleChange() {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <input type="password" onChange={handleChange} />
     )
 }
 
 export default Keypad;
+
+//In this lab we are adding the function keypad 
+// Afterwards we are adding a function to handle the click or event listener which in this case is the change event 
+//Then it wants us to add an input where the onChange will ne entered. We will also be adding the <input type='password'/> and adding in the onChange into that field 
+//Then we add a console log with the given text
